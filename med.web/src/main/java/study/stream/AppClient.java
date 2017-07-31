82d3a08bf868482946e426f06cf163642b373c8f
meddkim <meddkim@gmail.com>
2017-07-31 10:14:50 +0800

    some modified

---
+++ med.web/src/main/java/study/stream/AppClient.java
@@ -0,0 +1,37 @@
+package study.stream;
+
+/**
+ * @author: Administrator
+ * @date : 2017/3/6 0006
+ * @Description:
+ */
+public class AppClient {
+
+    private String client_name;
+    private String client_id;
+    private String client_secret;
+
+    public String getClient_name() {
+        return client_name;
+    }
+
+    public void setClient_name(String client_name) {
+        this.client_name = client_name;
+    }
+
+    public String getClient_id() {
+        return client_id;
+    }
+
+    public void setClient_id(String client_id) {
+        this.client_id = client_id;
+    }
+
+    public String getClient_secret() {
+        return client_secret;
+    }
+
+    public void setClient_secret(String client_secret) {
+        this.client_secret = client_secret;
+    }
+}
---
+++ med.web/src/main/java/study/stream/Demo.java
@@ -0,0 +1,25 @@
+package study.stream;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.UUID;
+
+/**
+ * @author: Administrator
+ * @date : 2017/3/5 0005
+ * @Description:
+ */
+public class Demo {
+    public static void main(String[] args) {
+//        System.out.println(System.getProperty("user.dir"));
+//        ERP 设计师平台 运营平台 选材中心
+//        AppClient appClient = new AppClient();
+//        appClient.setClient_name("ERP");
+//        appClient.setClient_id(UUID.randomUUID().toString());
+//        appClient.setClient_secret(UUID.randomUUID().toString());
+
+        System.out.println(UUID.randomUUID().toString());
+    }
+}
